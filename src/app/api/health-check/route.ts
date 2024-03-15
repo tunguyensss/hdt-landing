1fa7146985510fcf48eae9c4324a8c98e5c98dd6
tunv <nguyenvantu191298@gmail.com>
2024-03-15 22:36:19 +0700

    feat: oke

--- src/app/api/health-check/route.ts
+++ src/app/api/health-check/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export function GET(req: NextRequest) {
+export async function GET(req: NextRequest) {
   return NextResponse.json({
     message: "OK",
   });
--- src/components/header/nav.mobile.tsx
+++ src/components/header/nav.mobile.tsx
@@ -1,3 +1,4 @@
+"use client";
 interface NavMobileProps {}
 export default function NavMobile(props: NavMobileProps) {
   return (
